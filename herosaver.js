444b04979e1aa431cb3011f4e76f5bd1e053544a
Kiruke <kirukewalker@gmail.com>
2020-03-27 13:15:19 +0100

    Update herosaver.js

--- herosaver.js
+++ herosaver.js
@@ -145,17 +145,16 @@ function init() {
                                                 }
 
                                                 for (var k = 0; k < 4; k++) {
-							
-							if (geometry.morphTargets !== 'undefined') {
+                                                    if (geometry.morphTargets !== 'undefined') {
                                                         var tempVector = new THREE.Vector4(morphVector.x, morphVector.y, morphVector.z);
                                                     } else {
                                                         var tempVector = new THREE.Vector4(vector.x, vector.y, vector.z);
-                                                    }
+                                                    }                                                    
+                                                    
                                                     tempVector.multiplyScalar(weights[k]);
                                                     //the inverse takes the vector into local bone space
-							//which is then transformed to the appropriate world space
-                                                    tempVector.applyMatrix4(inverses[k]).applyMatrix4(skinMatrices[k]);                                             
-                                                    
+													//which is then transformed to the appropriate world space
+                                                    tempVector.applyMatrix4(inverses[k]).applyMatrix4(skinMatrices[k]);
                                                     finalVector.add(tempVector);
 
                                                 }
